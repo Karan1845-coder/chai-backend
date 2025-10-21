181ffdaecf4bab0ddd9233b2262f4ad524f5fd3c
Karan1845-coder <karansingh40p@gmail.com>
2025-10-21 18:09:12 +0530

    add user and video model

---
+++ src/models/user.model.js
@@ -0,0 +1,105 @@
+import mongoose ,{Schema} from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const userSchema=new Schema({
+  username:{
+    type:String,
+    required:true,
+    unique:true,
+    trim:true,
+    lowercase:true,
+    index:true,
+  },
+
+  email:{
+      type:String,
+      required:true,
+      unique:true,
+      lowercase:true,
+      trim:true,
+  },
+
+  fullname:{
+       type:String,
+       required:true,
+       trim:true,
+       index:true,
+  },
+  avatar:{
+    type:String, //cloudinary url
+    required:true,
+  },
+  coverImage:{
+    type:String,
+  
+  },
+  watchHistory:{
+    type:Schema.Types.ObjectId,
+    ref:"video"
+  },
+  password:{
+    type:String,
+    required:[true,"password is required"],
+
+  },
+  refreshToken:{
+    type:String,
+
+  },
+ 
+
+},
+ {
+     timestamps:true,
+  },
+)
+//password encryption kr dega
+userSchema.pre("save", async function (next) {
+  if(!this.isModified("password")) return next();
+
+  this.password=bcrypt.hash(this.password,10)
+  next()
+})
+//custom methods
+userSchema.methods.isPasswordCorrect= async function (password) {
+ return await bcrypt.compare(password,this.password)
+  
+}
+
+userSchema.methods.generateAccessToken=function(){
+ //sign in token
+    return jwt.sign({
+    _id:this._id,
+    email:this.email,
+    username:this.username,
+    fullname:this.fullname
+
+
+     
+
+  },
+process.env. ACCESS_TOKEN_SECRET,{
+  expiresIn:process.eventNames.ACCESS_TOKEN_EXPIRY
+})
+}
+userSchema.methods.generateAccessToken=function(){
+  return jwt.sign({
+    _id:this._id,
+    email:this.email,
+    username:this.username,
+    fullname:this.fullname
+
+
+     
+
+  },
+process.env. REFRESH_TOKEN_SECRET,{
+  expiresIn:process.eventNames.REFRESH_TOKEN_EXPIRY
+})
+}
+
+
+
+export const User=mongoose.model("User",userSchema)
+
